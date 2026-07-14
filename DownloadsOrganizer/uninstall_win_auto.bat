f0c18ff7ddbeeb8661c6cbbad15fe8821bb0645c
Peluboy <imulep2104@gmail.com>
2026-07-14 02:30:26 -0500

    Remove redundant DownloadsOrganizer subfolder

--- DownloadsOrganizer/uninstall_win_auto.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Uninstalling background task...
-schtasks /delete /tn "DownloadsOrganizer" /F
-echo --------------------------------------------------------
-echo Uninstalled! The Downloads Organizer will no longer run automatically.
-echo You can now close this window.
-echo --------------------------------------------------------
-pause
